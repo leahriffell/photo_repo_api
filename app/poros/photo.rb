91b93de68c1e543f866dbb686d673c960fa3cb7b
Leah Riffell <leah.riffell@gmail.com>
2021-01-13 11:09:26 -0700

    Add photo poro and spec

---
+++ app/poros/photo.rb
@@ -0,0 +1,13 @@
+class Photo
+  attr_reader :description,
+              :url,
+              :artist_name,
+              :artist_profile
+
+  def initialize(details)
+    @description = details[:description]
+    @url = details[:urls][:regular]
+    @artist_name = details[:user][:name]
+    @artist_profile = details[:user][:links][:html]
+  end
+end
