a822bfbfb609413e7343709ee9c4dd8b92cea2c3
Leah Riffell <leah.riffell@gmail.com>
2021-01-16 12:35:27 -0700

    Address rubocop violations

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,6 +4,7 @@ module Types
     field :create_trip, mutation: Mutations::Trips::CreateTrip, description: 'Create a new trip'
 
     # Photos
-    field :add_photo_to_trip, mutation: Mutations::Photos::AddPhotoToTrip, description: 'Add an Unsplash photo to a trip'
+    field :add_photo_to_trip, mutation: Mutations::Photos::AddPhotoToTrip,
+                              description: 'Add an Unsplash photo to a trip'
   end
 end
--- spec/factories/photo.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :photo do
-    description { Faker::Hipster.sentence }
-    url { Faker::Avatar.image }
-    artist {{ name: Faker::Artist.name, profile: Faker::Internet.url }} 
-  end
-end
