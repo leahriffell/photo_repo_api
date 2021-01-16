21fef1465997c4936bbcaec57a7ebfae61ae1ac1
Leah Riffell <leah.riffell@gmail.com>
2021-01-16 13:55:36 -0700

    Create photo test factory

--- spec/factories/photo_details.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :photo_details do
-    description { Faker::Hipster.sentence }
-    url { Faker::Avatar.image }
-    artist {{ name: Faker::Artist.name, profile: Faker::Internet.url }} 
-  end
-end
