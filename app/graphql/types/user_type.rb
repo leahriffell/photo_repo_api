d408e1971b1d3865b5265028c8d86b6a2a93ce49
Leah Riffell <leah.riffell@gmail.com>
2021-01-16 21:26:27 -0700

    Update user type to include photos and trips

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -2,5 +2,7 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String, null: false
+    field :photos, [Types::PhotoType], null: false
+    field :trips, [Types::TripType], null: false
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,5 +8,14 @@ FactoryBot.define do
         create_list(:trip, 3, user: user)
       end
     end
+
+    trait :with_photo_trip do
+      after(:create) do |user|
+        trips = create_list(:trip, 2, user: user)
+        trips[0].photos << create(:photo, user_uploaded: true)
+        trips[0].photos << create(:photo, user_uploaded: false)
+        trips[1].photos << create(:photo, user_uploaded: true)
+      end
+    end
   end
 end
