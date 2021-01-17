be3f1187bedfbdd55d663ab0d407faea6b67083e
Leah Riffell <leah.riffell@gmail.com>
2021-01-16 19:53:00 -0700

    Add top trips model method and spec

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,11 @@ class Trip < ApplicationRecord
   has_many :photos, through: :photo_trips
 
   validates :destination, presence: true
+
+  def self.top_trips(limit)
+    Trip.select('COUNT(destination) AS num_saved_trips, destination')
+        .group('destination')
+        .order('num_saved_trips DESC, destination')
+        .limit(limit)
+  end
 end
--- spec/models/trip_spec.rb
+++ spec/models/trip_spec.rb
@@ -10,4 +10,24 @@ RSpec.describe Trip, type: :model do
   describe 'validations' do
     it { should validate_presence_of :destination }
   end
+
+  describe 'class methods' do
+    describe 'top_trips(limit)' do
+      it 'returns most popular trip destinations' do
+        10.times { create(:trip, destination: 'Paris') }
+        7.times { create(:trip, destination: 'Spain') }
+        5.times { create(:trip, destination: 'Da Nang, Vietnam') }
+
+        top_three = Trip.top_trips(3)
+        expect(top_three.length).to eq(3)
+        expect(top_three[0][:destination]).to eq('Paris')
+        expect(top_three[1][:destination]).to eq('Spain')
+        expect(top_three[2][:destination]).to eq('Da Nang, Vietnam')
+
+        top_one = Trip.top_trips(1)
+        expect(top_one.length).to eq(1)
+        expect(top_one[0][:destination]).to eq('Paris')
+      end
+    end
+  end
 end
