5f5c26e6ba8313430bcd3d788e6fc5e775306542
Leah Riffell <leah.riffell@gmail.com>
2021-01-16 21:25:49 -0700

    Add user's photos and photo_trip association to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :trips, dependent: :destroy
+  has_many :photo_trips, through: :trips
+  has_many :photos, through: :photo_trips
 
   validates :email, uniqueness: true, presence: true
   validates :password_digest, presence: true
