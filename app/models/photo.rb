e736722a5eb7ab51cf279a40bed54aab1f589c57
Leah Riffell <leah.riffell@gmail.com>
2021-01-17 15:02:19 -0700

    Add apollo upload server and active storage

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,7 @@
 class Photo < ApplicationRecord
   has_many :photo_trips, dependent: :destroy
   has_many :trips, through: :photo_trips
+  has_one_attached :user_photo
 
-  validates :url, presence: true
+  attr_accessor :user_photo_url
 end
